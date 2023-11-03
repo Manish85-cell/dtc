8a2aed5a6f39351d3f726dfabf57d60e9eb8f6c2
Aman kumar <kumaraman58748@gmail.com>
2023-11-03 20:45:39 +0530

    myfirstcommit

--- bus/src/main.jsx
+++ bus/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+//import App from './App.jsx'
+import './index.css';
+import Home from './component/Home';
+import Bus from './component/Bus';
+import Layout from './component/Layout';
+import Login from './component/Login';
 
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='bus' element={<Bus/>}/>
+      <Route path='login' element={<Login/>}/>
+    </Route>
+    
+  )
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
