9cd03a2b491639ae9c0ff237092b974b0e86cfc8
Aman kumar <kumaraman58748@gmail.com>
2023-11-05 16:53:49 +0530

    bus

--- routes/models.py
+++ routes/models.py
@@ -25,35 +25,35 @@ class Bus(models.Model):
         return self.route
     def get_route_list(self):
         return self.route.split(",")  
-class Driver(models.Model):
-    D_id = models.IntegerField(primary_key=True)
-    D_name = models.CharField(max_length=64)
-    salary = models.IntegerField(validator=[
-        MinValueValidator(20,000),
-        MaxValueValidator(50,000),
-    ])
-    working_shift = models.CharField(max_length=64)
+# class Driver(models.Model):
+#     D_id = models.IntegerField(primary_key=True)
+#     D_name = models.CharField(max_length=64)
+#     salary = models.IntegerField(validator=[
+#         MinValueValidator(20,000),
+#         MaxValueValidator(50,000),
+#     ])
+#     working_shift = models.CharField(max_length=64)
     
-    def __str__(self):
-        return f"({self.D_id}) {self.D_name}"
-class Conductor(models.Model):
-    C_id = models.IntegerField(primary_key=True)
-    C_name = models.CharField(max_length=64)
-    salary = models.IntegerField(validators=[
-        MinValueValidator(15,000),
-        MaxValueValidator(30,000),
-    ])
-    working_shift = models.CharField(max_length=64)
+#     def __str__(self):
+#         return f"({self.D_id}) {self.D_name}"
+# class Conductor(models.Model):
+#     C_id = models.IntegerField(primary_key=True)
+#     C_name = models.CharField(max_length=64)
+#     salary = models.IntegerField(validators=[
+#         MinValueValidator(15,000),
+#         MaxValueValidator(30,000),
+#     ])
+#     working_shift = models.CharField(max_length=64)
     
-    def __str__(self):
-        return f"({self.D_id}) {self.D_name}"
+#     def __str__(self):
+#         return f"({self.D_id}) {self.D_name}"
 
-class Tickets(models.Model):
-    T_id = models.IntegerField(primary_key=True)
-    Bus_no = models.ForeignKey(Bus, on_delete=models.CASCADE, related_name="t_no")   
-    source = models.ForeignKey(BusStand, ondelete=models.CASCADE, related_name="dest")
-    destination = models.ForeignKey(BusStand, ondelete=models.CASCADE, related_name="src")
-    Fare = models.IntegerChoices()
+# class Tickets(models.Model):
+#     T_id = models.IntegerField(primary_key=True)
+#     Bus_no = models.ForeignKey(Bus, on_delete=models.CASCADE, related_name="t_no")   
+#     source = models.ForeignKey(BusStand, ondelete=models.CASCADE, related_name="dest")
+#     destination = models.ForeignKey(BusStand, ondelete=models.CASCADE, related_name="src")
+#     Fare = models.IntegerChoices()
 
 # class Passenger(models.Model):
 #     first = models.CharField(max_length=64)
