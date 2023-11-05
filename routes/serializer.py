3362b49ef76907971b261a5705b466626b02c956
Manish <mupreti2002@gmail.com>
2023-11-05 11:12:37 +0530

    updating reachable

--- routes/serializer.py
+++ routes/serializer.py
@@ -6,6 +6,12 @@ class BusSerializer(serializers.ModelSerializer):
         fields = ['code', 'Name']
 class RouteSerializer(serializers.ModelSerializer):
     source = serializers.StringRelatedField()
+    def to_representation(self, instance):
+        representation= super().to_representation(instance)
+        codes = representation['reachable'].split(',')
+        stands = [BusStand.objects.get(code=code).Name for code in codes]
+        representation['reachable'] = stands
+        return representation 
     class Meta:
         model = Routes
         fields = ['source', 'reachable']
